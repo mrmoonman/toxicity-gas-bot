0674283584c33bccf4d409da837ce96ca666eeff
Michael <Michael.taylor.reinecke@gmail.com>
2023-05-23 08:47:02 -0500

    Making code async and giving an option to use existing client if already running, modifying schedule to be every 15 minutes.

--- GweiBot/index.js
+++ GweiBot/index.js
@@ -3,29 +3,33 @@ const { Client, Events, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 module.exports = async function (context, myTimer) {
-  console.log("leggo");
   const discordToken = process.env.DISCORD_TOKEN;
-  client.login(discordToken);
-
   const CHANNEL_ID = "1068005248754532403"; // Channel ID to read from signup list
   const MESSAGE_ID = "1110430218713301032"; // message to read from emoji reactions
+  let messageSent = false;
+  const settings = {
+    apiKey: process.env.ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+  };
+
   client.on(Events.Error, async (e) => {
     console.log(e);
   });
   client.on(Events.ClientReady, async (c) => {
-    console.log("connected");
-    const settings = {
-      apiKey: process.env.ALCHEMY_API_KEY,
-      network: Network.ETH_MAINNET,
-    };
+    await getGweiAndSendMessage();
+  });
 
+  await client.login(discordToken);
+  if (client.isReady() == true && messageSent == false) {
+    await getGweiAndSendMessage();
+  }
+
+  async function getGweiAndSendMessage() {
     const alchemy = new Alchemy(settings);
     const hexGwei = await alchemy.core.getGasPrice();
     const gwei = Utils.formatUnits(hexGwei, "gwei");
-    if (parseInt(gwei) < 200) {
-      console.log("yeah we cheap");
+    if ((parseInt(gwei) < 45 || parseInt(gwei) > 80) && messageSent == false) {
       const channel = client.channels.cache.get(CHANNEL_ID);
-      console.log(JSON.stringify(channel));
       channel.messages.fetch(MESSAGE_ID).then(async (message) => {
         message.reactions.cache.each(async (reaction) => {
           // Check if the reaction matches the specified emoji
@@ -38,17 +42,22 @@ module.exports = async function (context, myTimer) {
             const userIDs = users.map((user) => user.id);
 
             const taggedUsers = userIDs.map((userID) => `<@${userID}>`);
-            const followUpMessage = `AYOOOO Gas is cheap, ${parseInt(
-              gwei
-            )} to be exact. ${taggedUsers.join(", ")}`;
+            const followUpMessage = `${
+              parseInt(gwei) < 45
+                ? `AYOOOO gas is cheap (${parseInt(
+                    gwei
+                  )} gwei), get those rerolls in.`
+                : `Gas is a little expensive right now (${parseInt(
+                    gwei
+                  )} gwei), wait a bit to reroll if you can.`
+            } ${taggedUsers.join(", ")}`;
 
             // Send the follow-up message
-            reaction.message.channel.send(followUpMessage);
-
-            context.done();
+            await reaction.message.channel.send(followUpMessage);
+            messageSent = true;
           }
         });
       });
     }
-  });
+  }
 };
